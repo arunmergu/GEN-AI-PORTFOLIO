6b811a968896698f3b4b090d1e8157c8ee0030f3
arunmergu <arun.mergu@gmail.com>
2024-07-24 16:16:48 +0100

    remove git icon from app

--- vertex-ai/gcp-x-formulae-hackathon/sustainability_scorecard/streamlit-app.py
+++ vertex-ai/gcp-x-formulae-hackathon/sustainability_scorecard/streamlit-app.py
@@ -128,13 +128,18 @@ car_model_options,make_list=get_make_and_models()
 #st.title("Car Sustainability Comparison")
 st.set_page_config(layout="wide")
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 def get_base64_of_image(img):
     with io.BytesIO() as buffer:
